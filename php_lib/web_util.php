fed5c6f44a623fd11b842cc57cc8fcdde5541901
Duke Leto <jonathan@leto.net>
2009-01-15 21:11:39 -0800

    Improvements to php is_search_request()

--- php_lib/web_util.php
+++ php_lib/web_util.php
@@ -1,15 +1,22 @@
-function is_search_request() {
-    $r = $_SERVER['HTTP_REFERER'];
-    $engines = array('/search?',
-                'images.google.',
-                'search.yahoo.com',
-                'del.icio.us/search',
-                'msn.com',
-                'live.com',
-                );
-    foreach ($engine as $eng) {
-        if (strpos($r,$eng) !== false)
-             return true;
+// Code to determine whether this is a search engine request
+// and set a cookie if it is
+$r = $_SERVER['HTTP_REFERER'];
+$engines = array('/search?',
+            'images.google.',
+            'search.yahoo.com',
+            'del.icio.us/search',
+            'msn.com',
+            'live.com',
+            );
+
+foreach ($engine as $eng) {
+    if (strpos($r,$eng) !== false) {
+        $search_request = 42;
+        setcookie('search_request', 42, time() + 4242, '/', '.leto.net');
     }
-    return false;
+}
+
+function is_search_request() {
+    global $search_request;
+    return ( $search_request == 42 || $_COOKIE['search_request'] == 42 );
 }
