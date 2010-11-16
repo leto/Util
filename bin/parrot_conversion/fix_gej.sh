019d1303b94ba5b5f3449fdc4e7d1ddbb932df0f
Jonathan "Duke" Leto <jonathan@leto.net>
2010-11-15 23:15:11 -0800

    Add another script used to fixup commits in the Parrot conversion

---
+++ bin/parrot_conversion/fix_gej.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+rev=$GIT_COMMIT
+
+if [ $rev = "1cec7c8c99bc446ccede7141a93a621d0f4e59d0" ]
+then
+    n="Géraud Continsouzas"; m="geraud@gcu.info";
+fi
+
+if [ $rev = "22840928853892a1b5ed020799702956d2f5ee76" ]
+then
+    n="Géraud Continsouzas"; m="geraud@gcu.info";
+fi
+
+if [ $rev = "cfb86a338b88a95606ca8361f3c0c2fc6210b384" ]
+then
+    n="Géraud Continsouzas"; m="geraud@gcu.info";
+fi
+
+if [ $rev = "498de4d5554f9d3e5dba6634e4e7c3c20c5ef28a" ]
+then
+    n="Géraud Continsouzas"; m="geraud@gcu.info";
+fi
+
+export GIT_AUTHOR_NAME="$n"
+export GIT_AUTHOR_EMAIL="$m"
+export GIT_COMMITTER_NAME="$n"
+export GIT_COMMITTER_EMAIL="$m"
+
