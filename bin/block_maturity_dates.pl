bbfd1f5e5d19866c63b89136a94cbff80f18aab3
Jonathan "Duke" Leto <jonathan@leto.net>
2018-06-19 15:01:01 -0700

    Fix off-by-one in total

--- bin/block_maturity_dates.pl
+++ bin/block_maturity_dates.pl
@@ -7,13 +7,13 @@ use POSIX qw(strftime);
 my $output = qx{./fiat/verus listtransactions |grep blockstomaturity | cut -d: -f2 | sed 's/,//g'};
 my @lines  = sort split /\n/, $output;
 
-my $num = 1;
+my $num = 0;
 for my $line (@lines) {
+    $num++;
     chomp $line;
     # my $date = localtime(time + 60*$line);
     my $date = strftime "%a %b %e %H:%M:00 %Y", localtime(time + 60*$line);
     print "$num $date\n";
-    $num++;
 }
 
 print "Total: $num immature blocks\n";
